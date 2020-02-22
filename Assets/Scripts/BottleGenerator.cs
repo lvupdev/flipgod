917640c8e10cc1aa33db57c2630797290a4308c5
Ddingjae418 <ssy008567naver.com>
2020-02-22 17:22:27 +0900

    다시 정리

---
+++ Assets/Scripts/BottleGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+물병을 생성하는 스크립트
+*/
+public class BottleGenerator : MonoBehaviour
+{
+    public GameObject bottlePrefab;
+    public Vector3 startPosition; //물병을 생성할 위치
+
+    public void GenerateBottle()
+    {
+        GameObject bottle = Instantiate(bottlePrefab) as GameObject;
+        bottle.transform.position = startPosition;
+    }
+}
---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/*
+게임 UI 관리 스크립트
+*/
+public class GameDirector : MonoBehaviour
+{
+    BottleController bottleController;
+    GameObject psychokinesis;
+    GameObject membraneCreater;
+    GameObject freezer;
+
+    void Start()
+    {
+        bottleController = GameObject.Find("BottlePrefab").GetComponent<BottleController>();
+        psychokinesis = GameObject.Find("Psychokinesis");
+        //membraneCreater = GameObject.Find("MembraneCreator");
+        //freezer = GameObject.Find("Freezer");
+    }
+
+    void Update()
+    {
+        if(bottleController.isSuperPowerAvailabe)
+    }
+
+    public void ShowPausePanel() // 일시정지 메뉴 창 보여주는 함수
+    {
+        GameObject.Find("UICanvas").transform.Find("PausePanel").gameObject.SetActive(true);
+    }
+
+    public void Retry() // 스테이지를 재시작하는 함수
+    {
+        SceneManager.LoadScene("Stage");
+    }
+
+    public void Resume() // 게임을 계속해서 진행하는 함수
+    {
+        GameObject.Find("UICanvas").transform.Find("PausePanel").gameObject.SetActive(false);
+    }
+
+    public void GoStageScene() // 스테이지 선택 화면으로 넘어가는 함수
+    {
+        SceneManager.LoadScene("SelectStage");
+    }
+
+    public void PlayerReselectBottle() // 상황에 맞춰 플레이 캐릭터가 물병을 재선택하도록 한다.
+    {
+        bottleController = GameObject.FindWithTag("Player").GetComponent<BottleController>();
+        GameObject presentPlayer = GameObject.FindWithTag("Player");
+        if (presentPlayer == psychokinesis) psychokinesis.GetComponent<KinesisController>().ReselectBottle();
+        //else if (presentPlayer == membraneCreater) membraneCreater.GetComponent<CreatorController>().ReselectBottle();
+        //else freezer.GetComponent<FreezerController>().ReselectBottle();
+    }
+
+}
