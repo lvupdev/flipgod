85add88dfb40ce3fbcc9f590145a8c0da669a73d
DdingJae418 <ssy008567@naver.com>
2020-07-20 22:59:46 +0900

    빙결자 필살기 구현 미완성 버전2

---
+++ Assets/Scripts/UIManager/ControllButtonsUIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * ControllButtons에 속하는 버튼들이 사라지고 나타나는 효과를 관리하는 스크립트입니다.
+ */
+public class ControllButtonsUIManager : MonoBehaviour
+{
+    private bool hideButtons;
+    private bool showButtons;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        hideButtons = false;
+        showButtons = false;
+    }
+
+    public void setHideButtons(bool value) { hideButtons = value; }
+    public void setShowButtons(bool value) { showButtons = value; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hideButtons)
+        {
+
+        }
+
+        if (showButtons)
+        {
+
+        }
+    }
+
+}
