93b26695f330da55fd21fc84d93e4c5bd12f906d
DdingJae418 <ssy008567@naver.com>
2020-08-21 19:00:56 +0900

    metalPlane 수정

--- Assets/Scripts/ingame/structure/MetalPlane2.cs
+++ Assets/Scripts/ingame/structure/MetalPlane2.cs
@@ -4,9 +4,43 @@ using UnityEngine;
 
 public class MetalPlane2 : Structure
 {
-    // Start is called before the first frame update
+    public bool isSpinning; //타일이 회전하는지의 여부
+    public float spinningSpeed;  //타일이 회전하는 속도
+    public bool isMoving; //타일이 이동하는지의 여부
+    public Vector2 direction; //이동방향
+    public float movingSpeed; //타일이 이동하는 속도
+    public float movingRange; //이동 범위
+
+    private Vector2 originPos; //처음 배치 위치
+    private int key; //이동 방향 전환 키
+
+    public new void Start()
+    {
+        base.Start();
+
+        isSpinning = false;
+        isMoving = false;
+        key = 1;
+        direction = Vector2.ClampMagnitude(direction, 1);
+    }
+
+
     void Update()
     {
         ThawDynamicStructure(isFreezed);
+
+        if (!isFreezed)
+            MoveDynamicStructure();
+    }
+
+    public void MoveDynamicStructure()
+    {
+        if(isSpinning)
+            transform.Rotate(new Vector3(0, 0, -360 * spinningSpeed * Time.deltaTime));
+
+        if (isMoving)
+        {
+
+        }
     }
 }
