ddc197e7c31bfd9e9aceec36f479002b3f7b1a55
DdingJae418 <ssy008567@naver.com>
2020-11-21 16:32:58 +0900

    Stage-3까지 작업

--- Assets/Scripts/ingame/structure/MetalPlane.cs
+++ Assets/Scripts/ingame/structure/MetalPlane.cs
@@ -2,27 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MetalPlane2 : Structure
+public class MetalPlane : Structure
 {
     public bool isSpinning; //타일이 회전하는지의 여부
     public float spinningSpeed;  //타일이 회전하는 속도
     public bool isMoving; //타일이 이동하는지의 여부
-    public Vector2 direction; //이동방향
+    public Vector3 direction; //이동방향
     public float movingSpeed; //타일이 이동하는 속도
     public float movingRange; //이동 범위
 
-    private Vector2 originPos; //처음 배치 위치
+    private Vector3 originPos; //처음 배치 위치
     private int key; //이동 방향 전환 키
+    private bool turnSucees; //성공적으로 방향을 전환했는지의 여부
 
     public new void Start()
     {
         base.Start();
 
-        isFreezable = true;
-        isSpinning = false;
-        isMoving = false;
         key = 1;
-        direction = Vector2.ClampMagnitude(direction, 1);
+        direction = Vector3.ClampMagnitude(direction, 1);
+        originPos = transform.position;
+        turnSucees = false;
     }
 
 
@@ -41,6 +41,21 @@ public class MetalPlane2 : Structure
 
         if (isMoving)
         {
+            transform.Translate(key * direction * movingSpeed * Time.deltaTime);
+
+            if((originPos - transform.position).magnitude > movingRange)
+			{
+				if (!turnSucees)
+				{
+                    key = -key;
+                    turnSucees = true;
+                }
+			}
+			else
+			{
+                turnSucees = false;
+			}
+
 
         }
     }
