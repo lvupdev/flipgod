96c79bf61d49528f47199715bda308fa6e542ed7
leegwae <leegwae@gmail.com>
2021-01-10 16:15:44 +0900

    Update texts of test buttons

--- Assets/Scripts/UIManager/SelectStageUIManager.cs
+++ Assets/Scripts/UIManager/SelectStageUIManager.cs
@@ -29,12 +29,16 @@ public class SelectStageUIManager : MonoBehaviour
 	{
 		textButton = Resources.Load<GameObject>("UI/Button_Test");
 
-		for (var i = 0; i < clearIndex + 2; i++)
+		for (var i = 0; i < clearIndex + 1; i++)
 		{
 			GameObject newButton = Instantiate(textButton);
+			StageData stageData = Resources.Load<StageData>("StageDatas/StageData-" + (i + 1));
 			newButton.transform.localPosition = Vector3.zero;
 			newButton.transform.SetParent(content, false);
 			newButton.transform.GetChild(0).GetComponent<Text>().text = "TEST#" + (i + 1);
+			newButton.transform.GetChild(2).GetComponent<Text>().text = "임시 텍스트";
+			newButton.transform.GetChild(4).GetComponent<Text>().text = "임시 시간 텍스트";
+			newButton.transform.GetChild(6).GetComponent<Text>().text = "임시 물병 텍스트";
 			newButton.transform.GetChild(7).GetComponent<Button>().onClick.AddListener(() => StartGame(i + 1));
 		}
 	}
@@ -52,5 +56,9 @@ public class SelectStageUIManager : MonoBehaviour
 	public void StartGame(int index)
 	{
 		SceneManager.LoadScene("Stage-" + index);
+		if (Time.timeScale == 0.0f)
+		{
+			Time.timeScale = 1.0f;
+		}
 	}
 }
